41a18fce42770fccbf562e8a21581ca22cee14b6
Max <egorovcotmax@gmail.com>
2023-02-03 19:02:28 +0400

    created notification for callback form

--- app/Http/Controllers/CallBackForm.php
+++ app/Http/Controllers/CallBackForm.php
@@ -5,18 +5,22 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\ContactFormRequest;
 use App\Models\Admin;
+use App\Notifications\CallbackMessage;
+use Notification;
 
 class CallBackForm extends Controller
 {
     
-    public function mailToAdmin(ContactFormRequest $request, Admin $admin) {
+    public function mailToAdmin(ContactFormRequest $message, Admin $admin) {
         
-        $validated = $request->validated();
+        $validated = $message->validated();
 
-        $admin_model = new \ReflectionClass(Admin::class);
+        // var_dump($message instanceof ContactFormRequest);
 
-        print_r($admin_model->getMethods());
+        // $admin_model = new \ReflectionClass(Admin::class);
+        // print_r($admin_model->getMethods());
 
-        return $validated;
+        $result = Notification::send($admin, new CallbackMessage($message));
+        return $result;
     }
 }
