d3077c71d8a864e0f5ec70ba55cf26950a863c81
Max <egorovcotmax@gmail.com>
2023-02-03 11:47:34 +0400

    added form request for validation data

--- app/Http/Controllers/CallBackForm.php
+++ app/Http/Controllers/CallBackForm.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\ContactFormRequest;
 
 class CallBackForm extends Controller
 {
     
-    public function mailToAdmin(Request $request) {
-        return $request->all();
+    public function mailToAdmin(ContactFormRequest $request) {
+        
+        $validated = $request->validated();
+
+        return $validated;
     }
 }
