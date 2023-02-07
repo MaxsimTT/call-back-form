c0f5cde887ff1b7d936791b2fa886309b0331666
Max <egorovcotmax@gmail.com>
2023-02-07 09:00:59 +0400

    Added Swagger-L5 version 3.1.0 for controller app/Http/Controllers/CallBackForm.php access http://127.0.0.1:8000/api/documentation

--- app/Http/Controllers/CallBackForm.php
+++ app/Http/Controllers/CallBackForm.php
@@ -9,13 +9,60 @@ use Notification;
 
 class CallBackForm extends Controller
 {
+
+    /**
+     * @OA\Post(
+     *     path="/api/contact",
+     *     tags={"call back form"},
+     *     summary="Sending call back form to admin email",
+     *     operationId="sendingCallBackForm",
+     *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 @OA\Property(
+     *                     property="name",
+     *                     type="string"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="email",
+     *                     type="email"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="message",
+     *                     type="text"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="phone",
+     *                     oneOf={
+     *                         @OA\Schema(type="string"),
+     *                         @OA\Schema(type="integer"),
+     *                     }
+     *                 ),
+     *                 example={"name": "Jessica Smith", "email": "example@qwe.com", "messsage": "Hello, World", "phone": 12345678}
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=201,
+     *         description="successful operation",
+     *     ),
+     *     @OA\Response(
+     *         response=403,
+     *         description="forbidden",
+     *     ),
+     * )
+     *
+     */
     
     public function mailToAdmin(ContactFormRequest $message) {
 
         if ($message->isMethod('POST')) {
 
             Notification::route('mail', config('admin.email'))->notify(new CallbackMessage($message->validated()));
-            return response()->json('Success', 200);
+            return response()->json('Success', 201);
         }
+
+        return response()->json('Forbidden', 403);
     }
 }
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,23 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *     description="This web application is designed to test interesting features of Laravel
+ ",
+ *     version="1.0.0",
+ *     title="Test web application",
+ *     termsOfService="http://swagger.io/terms/",
+ *     @OA\Contact(
+ *         email="egorovcotmax@gmail.com"
+ *     ),
+ * )
+ * @OA\Server(
+ *     description="API test this application",
+ *     url="http://127.0.0.1:8000"
+ * )
+ */
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
